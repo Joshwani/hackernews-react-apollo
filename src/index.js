a435cd1b74ebdb64fc9ead1f81f2f56ddc73e80e
Joshwani <jnielsen01@gmail.com>
2018-09-26 08:37:39 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,75 @@
+// importing the required dependencies from the installed packages.
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/index.css'
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { BrowserRouter } from 'react-router-dom'
+import { setContext } from 'apollo-link-context'
+import { AUTH_TOKEN } from './constants'
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+
+// create the httpLink that will connect your ApolloClient instance with the GraphQL API, 
+// your GraphQL server will be running on http://localhost:4000.
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+})
+
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem(AUTH_TOKEN)
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    }
+})
+
+// // instantiate ApolloClient by passing in the httpLink and a new instance of an InMemoryCache
+// const client = new ApolloClient({
+//     link: authLink.concat(httpLink),
+//     cache: new InMemoryCache()
+// })
+
+const wsLink = new WebSocketLink({
+    uri: `ws://localhost:4000`,
+    options: {
+        reconnect: true,
+        connectionParams: {
+        authToken: localStorage.getItem(AUTH_TOKEN),
+        }
+    }
+})
+
+const link = split(
+    ({ query }) => {
+        const { kind, operation } = getMainDefinition(query)
+        return kind === 'OperationDefinition' && operation === 'subscription'
+    },
+    wsLink,
+    authLink.concat(httpLink)
+)
+
+const client = new ApolloClient({
+    link,
+    cache: new InMemoryCache()
+})
+
+// render the root component of your React app
+// the App is wrapped with the higher-order component 
+// ApolloProvider that gets passed the client as a prop
+ReactDOM.render(
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </BrowserRouter>,
+    document.getElementById('root')
+  )
+registerServiceWorker()
